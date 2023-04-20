dc530c96004b72b2ba94e3fa6261979a969a2415
PatrizioTheDev <73086462+Patrizio-programs@users.noreply.github.com>
2023-04-20 00:55:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
 import streamlit as st
 
-# Hide the settings menu
-hide_streamlit_style = “”"
-
-#MainMenu {visibility: hidden;}
-footer {visibility: hidden;}
-
-“”"
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-
 
 # Setting the heading at the top of the page
 st.title('TeleChatGPT')
