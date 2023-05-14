00d372e4cf9cd68642461374a6d8e45c52245767
PatrizioTheDev <73086462+Patrizio-programs@users.noreply.github.com>
2023-05-13 20:32:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -93,8 +93,3 @@ def index():
   else:
     return render_template("index.html")
   return ("TELECHATGPT")
-
-
-# Start the Flask app
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
