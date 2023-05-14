50e056d1b374e877e6ffb1513370f0f23c2554c5
PatrizioTheDev <73086462+Patrizio-programs@users.noreply.github.com>
2023-05-14 00:45:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -103,8 +103,3 @@ def webhook():
   update = telebot.types.Update.de_json(request.stream.read().decode('utf-8'))
   bot.process_new_updates([update])
   return 'ok', 200
-
-
-# Start the server
-if __name__ == '__main__':
-  server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
