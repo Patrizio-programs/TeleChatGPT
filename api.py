c9a0398390db5d9c6a31bea5897db725f21ed209
PatrizioTheDev <73086462+Patrizio-programs@users.noreply.github.com>
2023-05-19 19:07:44 -0400

    Update api.py

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 import os
 
 completions = "https://api.pawan.krd/v1/chat/completions"
-api_key = os.enviton['API_KEY']
+api_key = os.environ['API_KEY']
 headers = {
   "Authorization": f"Bearer {api_key}",
   "Content-Type": "application/json"
