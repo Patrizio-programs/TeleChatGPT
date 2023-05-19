24c25827e3a5d8e4cf42ad94322058c98d5bcc92
PatrizioTheDev <73086462+Patrizio-programs@users.noreply.github.com>
2023-05-19 19:01:08 -0400

    Create api.py

---
+++ api.py
@@ -0,0 +1,8 @@
+import os
+
+completions = "https://api.pawan.krd/v1/chat/completions"
+api_key = os.enviton['API_KEY']
+headers = {
+  "Authorization": f"Bearer {api_key}",
+  "Content-Type": "application/json"
+}
