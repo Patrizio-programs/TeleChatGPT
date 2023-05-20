69d17b0b2554a462fbc509223c0c78d9ac80dd5e
PatrizioTheDev <73086462+Patrizio-programs@users.noreply.github.com>
2023-05-19 21:23:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -147,6 +147,7 @@ def index():
         json_string = flask.request.get_data().decode('utf-8')
         update = telebot.types.Update.de_json(json_string)
         bot.process_new_updates([update])
+        return ""
     return 'ok', 200
   else:
     return render_template("index.html")
