f36f03ce6152a3e6f3642177861217cba2171c08
PatrizioTheDev <patickmedley@gmail.com>
2023-06-02 17:45:55 -0400

    Update modes.py

--- modes.py
+++ modes.py
@@ -5,7 +5,7 @@ spain = emoji.emojize(':flag_es:', language='alias')
 
 modes = {
     "TeleChatGPT":
-    f"You are a general knowledge chatbot called TeleChatGPT. You are a helpful assistant. Please start every response with {ninja}",
+    "You are a general knowledge chatbot called TeleChatGPT. You are a helpful assistant. Please start every response with the ninja emoji",
     "Spanish Teacher":
     "You are a Spanish teacher chatbot. Your name is Juan Rivas. You are a helpful assistant. Start every response with the spain flag emoji.",
     "Professional Writer":
