9c494990a8bec50e1964669916f68d5330e9e711
PatrizioTheDev <73086462+Patrizio-programs@users.noreply.github.com>
2023-06-04 21:13:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,9 @@ from telebot import types
 from flask import Flask, request, render_template
 import requests
 import json
-# LLM imports
-from gpt_llm import llm as chatbot
+
 from modes import modes
-from langchain.prompts import (
-    SystemMessagePromptTemplate,
-    ChatPromptTemplate,
-    HumanMessagePromptTemplate,
-)
+import ai
 
 mode_names = list(modes.keys())
 current_mode = modes["TeleChatGPT"]
@@ -38,18 +33,8 @@ def generate_message(message):
     chat_id = message.chat.id
     prompt = message.text
     reply = bot.send_message(chat_id, "Thinking...")
-
-    # SORT OUT LLM CONFIG- NEED TO FIND A WAY TO SEPERATE THIS FUNCTION FOR BETTER READABILITY
-    system_message = current_mode
-    system_message_prompt = SystemMessagePromptTemplate.from_template(
-        system_message)
-    human_message = prompt
-    human_message_prompt = HumanMessagePromptTemplate.from_template(
-        human_message)
-    chat_prompt = ChatPromptTemplate.from_messages(
-        [system_message_prompt, human_message_prompt])
-    messages = chat_prompt.format_prompt().to_messages()
-    response = chatbot(str(chat_prompt.format_prompt()))
+    req = ai.Completion.create(prompt=prompt, systemMessage=current_mode)
+    response = req["text"]
     bot.edit_message_text(chat_id=chat_id,
                           message_id=reply.message_id,
                           text=response)
@@ -134,7 +119,7 @@ def index():
 
         return 'ok', 200
     else:
-        return render_template("index.html")
+        return ("working")
 
 
 # define callback function for mode buttons
