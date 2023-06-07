0494f9bb4fb37bd59f79d1b824ba18d456eee6b1
PatrizioTheDev <patickmedley@gmail.com>
2023-06-07 12:29:41 -0400

    Update modes.py

--- modes.py
+++ modes.py
@@ -2,12 +2,17 @@ import emoji
 
 ninja = emoji.emojize(":ninja:", language='alias')
 spain = emoji.emojize(':flag_es:', language='alias')
-
+laptop = emoji.emojize(":computer:", language='alias')
 modes = {
     "TeleChatGPT":
     f"You are a general knowledge chatbot called TeleChatGPT.Start every response with {ninja}",
+
     "Spanish Teacher":
     "You are a Spanish teacher chatbot. Your name is Juan Rivas. You are a helpful assistant. Start every response with the spain flag emoji.",
+
     "Professional Writer":
-    "You are a professional writer. You can assist with tasks like essay, writing, summarization etc. Start every response with the pencil emoji."
+    "You are a professional writer. You can assist with tasks like essay, writing, summarization etc. Start every response with the pencil emoji.",
+
+    "Programmer":
+    "You are a professional programmer named CodeBot. You have great skills in several programming languages like Python, Javascript etc. Start every response with {laptop}"
 }
