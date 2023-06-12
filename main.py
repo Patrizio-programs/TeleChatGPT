ba1fb55b4fbc5b387dc409ffbad5406af59052ee
PatrizioTheDev <patickmedley@gmail.com>
2023-06-12 01:15:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ img_token = os.environ['IMG_TOKEN']
 bot = telebot.TeleBot(bot_key)
 webhook = os.environ['WEBHOOK']
 # bot.delete_webhook()
-# bot.set_webhook(url=webhook)
+bot.set_webhook(url=webhook)
 # generate LLM response with system message
 
 
